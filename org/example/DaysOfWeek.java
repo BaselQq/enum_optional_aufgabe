9c25acef83f9dc4bc309ff2d4f013cbe3a0ee81c
admin <baselqarb@gmail.com>
2024-03-25 14:39:05 +0100

    fix getdaytype function

--- org/example/DaysOfWeek.java
+++ org/example/DaysOfWeek.java
@@ -18,27 +18,4 @@ public enum DaysOfWeek {
     DaysOfWeek(String day) {
         this.day = day;
     }
-
-    public String getDayType(String day) {
-        if (day.equals(MONDAY) || day.equals(TUESDAY)
-                || day.equals(THURESDAY)
-                || day.equals(WEDNESDAY)
-                || day.equals(FRIDAY)
-        ) {
-            return day.toString();
-        }
-
-        if (!day.equals(this.day)) {
-            return "You need to pass a day as String";
-        }
-
-        return "Weekend";
-    }
-
-    @Override
-    public String toString() {
-        return "org.example.Main.DaysOfWeek{" +
-                "day='" + day + '\'' +
-                '}';
-    }
 }
